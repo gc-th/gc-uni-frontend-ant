ff6d18e9a359922efc74bf1c04489a578a2f0903
Thomas <thomas@Thomass-MBP.fritz.box>
2020-03-07 16:24:25 +0100

    added possibilty to create less theme file

--- config-overrides.js
+++ config-overrides.js
@@ -1,14 +1,36 @@
 const { override, fixBabelImports, addLessLoader } = require('customize-cra')
+const VariablesOutput = require('less-plugin-variables-output');
+const fs = require('fs');
+const path = require('path');
+const less = require('less');
+
+const themeLessFileName = path.resolve(path.join(__dirname, 'src', 'theme.less'));
+const lessContent = fs.readFileSync(themeLessFileName, 'utf8');
+let lessOverrides = {};
+
+const lessPluginOptions = {
+  callback: (vars) => lessOverrides = vars
+};
+
+less.render (lessContent, {
+  plugins: [
+    new VariablesOutput(lessPluginOptions)
+  ]
+}, (error, output) => {
+  if (error) {
+    throw new Error('Error occured during the parsing of the theme.less file: ' + error);
+    return config;
+  }
+});
 
 module.exports = override( fixBabelImports('import', {
   libraryName: 'antd',
   libraryDirectory: 'es',
   style: true,
   }),
+  
   addLessLoader({
     javascriptEnabled: true,
-    modifyVars: { 
-      '@primary-color': '#1DA57A' 
-    },
+    modifyVars: lessOverrides
   }),
 )
