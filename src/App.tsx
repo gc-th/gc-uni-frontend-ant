a493670a4880214ceea944636dd20f1022134484
Thomas <thomas@Thomass-MBP.fritz.box>
2020-03-07 13:03:48 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import CreateExercise from './pages/CreateExercise'
+function App() {
+  return (
+    <CreateExercise />
+  )
+}
+
+export default App
---
+++ src/pages/CreateExercise.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import CreateExerciseWizard from '../containers/Exercise/CreateExerciseWizard/CreateExerciseWizard'
+
+class CreateExercise extends Component {
+  render() {
+    return(
+      <CreateExerciseWizard />
+    )
+  }
+}
+
+export default CreateExercise
